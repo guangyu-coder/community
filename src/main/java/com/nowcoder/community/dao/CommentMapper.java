af58607125d87d9f3db984ece810f2cb1f475ba8
liguangyu1995 <172976475@qq.com>
2020-06-22 10:47:48 +0800

    commit

---
+++ src/main/java/com/nowcoder/community/dao/CommentMapper.java
@@ -0,0 +1,22 @@
+package com.nowcoder.community.dao;
+
+import com.nowcoder.community.entity.Comment;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
+/**
+ * @author liguangyu
+ * @create 2020/6/20 16:44
+ * classname
+ */
+
+@Mapper
+public interface CommentMapper {
+
+    List<Comment> selectCommentByEntity(int entityType, int entityId, int offset, int limit);
+
+    int selectCountByEntity(int entityType, int entityId);
+
+    int insertComment(Comment comment);
+}
